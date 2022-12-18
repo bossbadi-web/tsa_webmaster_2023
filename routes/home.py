022a7aeca86dd5d264c832004eeffc599ddb7982
Andy Tang <bossbadi123@gmail.com>
2022-12-17 20:05:21 -0500

    Add pages for different categories

--- routes/home.py
+++ routes/home.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, Blueprint
 
 home = Blueprint('home', __name__)
 
@@ -6,6 +6,31 @@ home = Blueprint('home', __name__)
 @home.route('/', methods=['GET'])
 def home_():
     return render_template('home.html')
+
+
+@home.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+@home.route('/experience', methods=['GET'])
+def experience():
+    return render_template('experience.html')
+
+
+@home.route('/pricing', methods=['GET'])
+def pricing():
+    return render_template('pricing.html')
+
+
+@home.route('/spaceship', methods=['GET'])
+def spaceship():
+    return render_template('spaceship.html')
+
+
+@home.route('/training', methods=['GET'])
+def training():
+    return render_template('training.html')
 
 
 def setup(app):
